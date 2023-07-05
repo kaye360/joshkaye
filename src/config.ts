c0ebcc2c959be55b5e0c0a8ab10c8ad2e86ec4a6
joshkaye88 <jkaye@shaw.ca>
2023-07-05 12:59:27 -0700

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,114 @@
+/**
+ * 
+ * Site Configuration
+ * 
+ */
+
+
+/**
+ * Site Info
+ */
+
+export const SITE_FULL_NAME = 'Full Cool Site Name Ltd.'
+
+export const SITE_SHORT_NAME = 'Cool Site'
+
+export const LOGO_NAME = SITE_SHORT_NAME
+
+export const PHONE_NUMBER = '604-533-1234'
+
+export const EMAIL_ADDRESS = 'email@email.com'
+
+export const LOCATION = 'Port Coquitlam, BC'
+
+export const HEADER_LOGO_PATH = '/img/logo.svg'
+
+export const HERO_IMG_PATH = '/img/hero.webp'
+
+export const FAVICON_PATH = '/meta-img/favicon.svg'
+
+
+/**
+ * Navigation Links
+ */
+
+export interface Navlink {
+    title     : string,
+    path      : string,
+    mainNav   : boolean,
+    footerNav : boolean,
+    cta       : boolean
+}
+
+/** Note: There can only be 1 Call to Action link */
+export const NAV_LINKS : Navlink[] = [
+    {
+        title     : 'Home',
+        path      : '/',
+        mainNav   : true,
+        footerNav : true,
+        cta       : false
+    },
+    {
+        title     : 'About',
+        path      : '/about',
+        mainNav   : true,
+        footerNav : true,
+        cta       : false
+    },
+    {
+        title     : 'Contact',
+        path      : '/contact',
+        mainNav   : true,
+        footerNav : true,
+        cta       : false
+    },
+    {
+        title     : 'Privacy Policy',
+        path      : '/privact',
+        mainNav   : false,
+        footerNav : true,
+        cta       : false
+    },
+    {
+        title     : 'Call to Action',
+        path      : '/cta',
+        mainNav   : true,
+        footerNav : true,
+        cta       : true
+    },
+]
+
+export interface Contactlink {
+    title : string,
+    icon? : string,
+    path?  : string
+}
+
+export const CONTACT_LINKS: Contactlink[] = [
+    {
+        title : PHONE_NUMBER,
+        icon  : 'phone_iphone',
+        path  : 'tel:' + PHONE_NUMBER
+    },
+    {
+        title : EMAIL_ADDRESS,
+        icon  : 'email',
+        path  : 'mailto:' + EMAIL_ADDRESS
+    },
+    {
+        title : LOCATION,
+        icon  : 'place',
+    },
+]
+
+
+/**
+ * General Types
+ */
+
+export interface Layout {
+    title : string,
+    description : string,
+    class? : string
+}
